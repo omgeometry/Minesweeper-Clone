487771f9dc7da07d375b81520f2edc4ad9172ba8
CarterHoltmann <cartermholtmann@gmail.com>
2024-11-21 14:35:44 -0500

    Real cell.h

--- cell.h
+++ cell.h
@@ -1,1 +1,17 @@
-// Carter 
+// Cell object
+// Project Minesweeper Clone
+
+
+#pragma once
+
+class Cell {
+
+public:
+  // take in bool and depending on left or right click, do different things
+  Cell(bool k); // constructor, bool true = bomb/ false = not bomb
+
+private:
+
+  bool isFlag(); // to check flagged spaces
+  bool isBomb(); // check for bomb
+};
